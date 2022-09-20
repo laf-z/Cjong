931997be987ac0fe272a5876e41c8f2f2f50acab
vitorrobertogq <105520604+vitorrobertogq@users.noreply.github.com>
2022-09-20 19:35:02 -0300

    Add files via upload

---
+++ cronometro.c
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <windows.h>
+
+
+void cronometro(int dificuldade) {
+    
+    for(float time = 240*dificuldade ; time > 0; time = time - 0.1){
+    wprintf(L"%.1f\n",time);
+    Sleep(100);
+    }}
+
+void main (){
+    _setmode(_fileno(stdout), 0x00020000);
+    cronometro(2);
+}
+
+
+
+
